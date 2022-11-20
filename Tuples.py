2f011756037e11fdd81236b20d40e380bf27f157
klaus19 <tejaskhartude@gmail.com>
2022-11-20 20:28:03 +0530

    tuples methods

---
+++ Tuples.py
@@ -0,0 +1,16 @@
+# Tuples are collections that are ordered and immutable,
+# meaning that their content cannot be changed.
+
+vehicles = ("Computer", "Smartphone", "Smart watch", "Tablet")
+
+print(vehicles)
+
+# length
+print(len(vehicles))
+
+# access the third index
+print(vehicles[3])
+
+# slicing
+
+print(vehicles[:3])
