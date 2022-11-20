cfbf48644843efe8316c36b7f619da121947b4a6
klaus19 <tejaskhartude@gmail.com>
2022-11-20 15:02:20 +0530

    data types in python

--- DataType.py
+++ DataType.py
@@ -2,3 +2,15 @@
 
 print(True * 3)
 print(False * 4)
+
+# complex numbers
+
+complex_number = 1 + 6j
+print(complex_number)
+
+# Strings in python
+
+name = "Tejas"  # double quote
+name = 'Tejas'  # single quote
+
+
